e4bb00fd8bcf94cf4f7af621fd74b0b0ebab4a04
Runsz <karuniaraharjo06@gmail.com>
2022-11-10 20:36:13 +0700

    Penambahan Halaman Hasil <Arun>

---
+++ src/features/Hal-Hasil/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const HalHasil = () => {
+    return (
+        <div className="mx-auto text-center p-3">
+            <div className="bg-gray-400 p-20 font-bold mx-auto rounded-3xl">
+                <h1>HASIL VOTING</h1>
+                <img src="" alt="JPG" />
+            </div>
+            
+            <div  className="bg-gray-400 p-16 font-bold mx-auto mt-10 rounded-3xl">
+                <h1>Osis</h1>
+                <img src="" alt="Osis"/>
+            </div>
+            <div className="bg-gray-400 p-16 font-bold mx-auto mt-10 rounded-3xl">
+                <h1>Pradana Putra</h1>
+                <img src="" alt="Pradana Putra" />
+            </div>
+            <div className="bg-gray-400 p-16 font-bold mx-auto mt-10 rounded-3xl">
+                <h1>Pradana Putri</h1>
+                <img src="" alt="Pradana Putri" />
+            </div>
+            <button className=" bg-cyan-400 mt-10 p-2 px-10 text-white rounded-2xl">HOME</button>
+        </div>
+    )
+}
+
+export default HalHasil
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import App from './features/App'
 import routes from './routes'
 import './index.css'
 import KPULogin from './features/KPULogin'
+import HalHasil from './features/Hal-Hasil'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <KPULogin/>
+    <HalHasil/>
   </React.StrictMode>
 )
