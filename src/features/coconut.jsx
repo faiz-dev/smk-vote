4c856a8f9082230e438edba3d81a12ae62e49648
alfa423 <rizkipurlani@gmail.com>
2022-11-11 09:18:44 +0700

    hai

--- src/features/coconut.jsx
+++ src/features/coconut.jsx
@@ -1,2 +1,13 @@
 import React from "react";
-const Vote = () => {} 
+const Vote = () => {
+    return (
+        <><h1 className="text-center text-lg">VOTING</h1>
+        <h2 className="text-lg">Pramuka</h2>
+        <img src="iykh.jpg" className="inline" />
+        <img src="iykh.jpg" className="inline" />
+        <img src="iykh.jpg" className="inline" /></>
+        
+
+    )
+} 
+export default Vote
--- src/features/votingPramuka/cocnut.jsx
+++
@@ -1,5 +0,0 @@
-p bhanh ini dari roji masa depan 
-import React from "react"
-const Vote = () {
-    react()
-}
