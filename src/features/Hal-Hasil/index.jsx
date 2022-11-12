8a563023e07f98deb061f83256ef4c14d4704f42
Faiz <a.faizmail@gmail.com>
2022-11-12 20:20:07 +0700

    modif script arun, coba pahami dan modifikasi

--- src/features/Hal-Hasil/index.jsx
+++ src/features/Hal-Hasil/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const HalHasil = () => {
+    const dataVotes = [
+        {
+            id: 1,
+            name: 'OSIS',
+            img: 'https://dummyimage.com/300x100/81A9B8/fff&text=OSIS'
+        }
+    ]
+    
     return (
         <div className="mx-auto text-center p-3 md:grid grid-cols-3 grid-rows-2">
             <div className="bg-gray-400 p-20 font-bold mx-auto rounded-3xl py-25 md:col-span-3 md:w-full">
@@ -10,13 +18,18 @@ const HalHasil = () => {
                 <img src="" alt="JPG" loading="lazy" />
             </div>
             
-            <div  className="bg-gray-400 p-16 font-bold mx-auto mt-10 rounded-3xl md:mt-20 md:w-4/5 cursor-pointer hover:bg-gray-500">
-                <h1>
-                    Osis
-                </h1>
-                <img src="" alt="Osis" loading="lazy" />
-            </div>
-            <div className="bg-gray-400 p-16 font-bold mx-auto mt-10 rounded-3xl md:mt-20 md:w-4/5 cursor-pointer hover:bg-gray-500">
+            {dataVotes.map(v => (
+                <div 
+                    className="bg-gray-400 p-16 font-bold mx-auto mt-10 rounded-3xl md:mt-20 md:w-4/5 cursor-pointer hover:bg-gray-500"
+                    key={v.id}>
+                    <h1>
+                        {v.name}
+                    </h1>
+                    <img src={v.img} alt={v.name} loading="lazy" className="w-full" />
+                </div>
+            ))}
+
+            {/* <div className="bg-gray-400 p-16 font-bold mx-auto mt-10 rounded-3xl md:mt-20 md:w-4/5 cursor-pointer hover:bg-gray-500">
                 <h1>
                     Pradana Putra
                 </h1>
@@ -27,7 +40,7 @@ const HalHasil = () => {
                     Pradana Putri
                 </h1>
                 <img src="" alt="Pradana Putri" loading="lazy" />
-            </div>
+            </div> */}
             <button className="bg-cyan-400 mt-10 p-2 px-10 mx-auto text-white rounded-xl md:mt-20 md:col-start-2 md:hover:bg-cyan-500">
                 HOME
             </button>
