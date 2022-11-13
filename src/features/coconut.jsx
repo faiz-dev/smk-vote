39cc09a401b66a7c303c31a3142fda421d0f1ebb
alfa423 <rizkipurlani@gmail.com>
2022-11-13 19:42:11 +0700

    jalo

--- src/features/coconut.jsx
+++ src/features/coconut.jsx
@@ -2,10 +2,11 @@ import React from "react";
 const Vote = () => {
     return (
         <><h1 className="text-center text-lg">VOTING</h1>
-        <h2 className="text-lg">Pramuka</h2>
-        <img src="iykh.jpg" className="inline" />
-        <img src="iykh.jpg" className="inline" />
-        <img src="iykh.jpg" className="inline" /></>
+        <h2 className="text-lg font-bold">Pramuka</h2>
+        <img src="iykh.jpg" className="inline rounded-lg" />
+        <img src="iykh.jpg" className="inline rounded-lg" />
+        <img src="iykh.jpg" className="inline rounded-lg" />
+        <button className="bg-cyan-500 rounded-xl decoration-white">KIRIM</button></>
         
 
     )
