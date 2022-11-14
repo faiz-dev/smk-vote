afc7ad9fa587e4e85988823181dd548ee7a5b17a
hrdityap23 <pherditya@gmail.com>
2022-11-14 08:26:34 +0700

    putra

--- src/features/Hal-Portal/index.jsx
+++ src/features/Hal-Portal/index.jsx
@@ -2,24 +2,30 @@ import React from "react";
 
 const HalPortal = () => {
     return (
-        <div className=" bg-slate-300 md:bg-white w-96 mx-auto">
-    <div class="">
-        <img src="src/features/Hal-Portal/menu.jpg" className="mx-auto rounded-3xl mt-20"/>
+     
+        <div class="">
+            <img src="src/features/Hal-Portal/menu.jpg" className="mx-auto rounded-3xl mt-20 w-80 h-40 md:w-5/6 h-96"/>
+            
+            <div class=" md:flex ml-40 "> 
+                <a href="#">
+                    <img className=" mx-auto rounded-3xl mt-10 cursor-pointer " src="https://dummyimage.com/300x50/13688f/fff.png&text=OSIS"/>
+                </a>
+                <a href="#" >
+                    <img className=" mx-auto rounded-3xl mt-10 cursor-pointer md:ml-96 " src="https://dummyimage.com/300x50/13688f/fff.png&text=PRADANA"/>
+                </a>
+            </div>
+            
+            <div className=" md:flex  ml-40">
+                <a href="#">
+                    <img className=" mx-auto rounded-3xl mt-6 cursor-pointer  " src="https://dummyimage.com/300x50/13688f/fff.png&text=PRADANI"/>
+                </a>
+
+                <a href="#">
+                    <img className=" mx-auto rounded-3xl mt-6 cursor-pointer md:ml-96  " src="https://dummyimage.com/300x50/13688f/fff.png&text=HASIL"/>
+                </a>
+            </div>
+        </div>    
         
-        <div class=" "> 
-            <a href="#">
-                <img className=" mx-auto rounded-3xl mt-10 cursor-pointer " src="https://dummyimage.com/300x50/13688f/fff.png&text=OSIS"/>
-            </a>
-            <a class="" href="#">
-                <img  className=" mx-auto rounded-3xl mt-6 cursor-pointer " src="https://dummyimage.com/300x50/13688f/fff.png&text=PRAMUKA"/>
-            </a>
-        </div>
-        
-        <a href="#">
-            <img className=" mx-auto rounded-3xl mt-6 cursor-pointer " src="https://dummyimage.com/300x50/13688f/fff.png&text=HASIL"/>
-        </a>
-    </div>    
-        </div>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import App from './features/App'
 import routes from './routes'
 import './index.css'
 import AuthProvider from './AuthProvider'
+import HalPortal from './features/Hal-Portal'
+import Sukses from './features/Sukses'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={routes}>  
-        <App/> 
-    </RouterProvider>
+    <HalPortal />
   </React.StrictMode>
 )
 
