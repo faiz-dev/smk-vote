35d3bc22e3ba999641181f15b30ec37eb65056c6
khayatul <khayatulkhasanah06@gmail.com>
2022-11-14 11:41:15 +0700

    halaman voting osis

--- src/features/VoteOsis/index.jsx
+++ src/features/VoteOsis/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 const VotingOsis = () => {
-    return (
-        <div className="text-center lg:bg-slate-200 rounded-2xl pt-100 py-10">
-            
-            <h1 className=" my-16 font-bold text-3xl">VOTING OSIS</h1>
-            <img src="https://dummyimage.com/333x193/000/0011ff"  className="mx-auto mt-10  md:inline-flex"/>
-            <img src="https://dummyimage.com/333x193/000/0011ff"  className="mx-auto mt-10  md:inline-flex"/>
-            <img src="https://dummyimage.com/333x193/000/0011ff"  className="mx-auto mt-10  md:inline-flex"/>
-            <button className=" bg-cyan-400 p-3 rounded-xl mt-10 mx-auto lg:display: block">Submit</button>
+    return(
+        <div className="">
+        <h1 className="font-bold text-center pt-16 text-3xl">VOTING OSIS</h1>
+        <p className="text-center">Klik gambar kandidat pilihan anda <br /> untuk memberi voting</p>
+        <div className="md:flex flex-row">
+            <img src="https://dummyimage.com/333x193/81a9b8/fff" className="rounded-3xl pb-1 mx-auto"/>
+            <img src="https://dummyimage.com/333x193/81a9b8/fff" className="rounded-3xl pb-1 mx-auto"/>
+            <img src="https://dummyimage.com/333x193/81a9b8/fff" className="rounded-3xl mx-auto"/>
         </div>
+        <button className="bg-[url('https://dummyimage.com/172x45/7BA9AC/fff&text=+')] text-white mt-5 mx-auto px-14 py-3 rounded-3xl md:display: block">Submit</button>
+    </div>
     )
 }
 
-export default VotingOsis
+export default VotingOsis
