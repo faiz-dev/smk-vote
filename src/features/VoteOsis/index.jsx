2f7f432126f253b81f890bbbaa44e8de6c6ef2a4
khayatul <khayatulkhasanah06@gmail.com>
2022-11-15 08:36:30 +0700

    atul

--- src/features/VoteOsis/index.jsx
+++ src/features/VoteOsis/index.jsx
@@ -1,42 +1,54 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import calon1 from '../../assets/Calon1.png'
+
+
 const VotingOsis = () => {
-    const judulVoting = 'Voting Pradana Putra'
-    const candidates = [
-        {
-            id: 1,
-            name: 'Rahmat & Rahmadhani',
-            img: calon1,
-            selected: false
-        },
-        {
-            id: 2,
-            name: 'Andika & Rahmat',
-            img: 'https://dummyimage.com/333x193/81a9b8/fff?text=Andika-Rahmat',
-            selected: true
-        },
-        {
-            id: 3,
-            name: 'Ramadhani & Andika',
-            img: 'https://dummyimage.com/333x193/81a9b8/fff?text=Andika-Rahmat',
-            selected: false
-        }
-    ]
+
+    const [JudulVoting, setJudulVoting] = useState([])
+    const [Voting, setVoting] = useState([])
+    const navigate = useNavigate()
+    const goToHasil = () => {
+        navigate('/sukses')
+    }
+
+
+    useEffect(() => {
+        (async () => {
+            const result = await axios.get('http://116.197.129.178:8083/api/periode/4')
+                .then((response) => response.data)
+            setJudulVoting(result)
+        })()
+    }, [])
+
+    
+    useEffect(() => {
+        (async () => {
+            const result = await axios.get('http://116.197.129.178:8083/api/calon/periode/4')
+                .then((response) => response.data)
+            setVoting(result)
+        })()
+    }, [])
+
     return(
         <div className="">
-            <h1 className="font-bold text-center mt-5 text-3xl">{judulVoting}</h1>
-            <p className="text-center mb-2">Klik gambar kandidat pilihan anda <br /> untuk memberi voting</p>
+            {JudulVoting.map(j =>(
+                <h1
+                className="font-bold text-center mt-5 mb-3 text-3xl">{j.name}</h1>
+            ))}
+            <p className="text-center mb-2 md:mb-5">Klik gambar kandidat pilihan anda <br /> untuk memberi voting</p>
             <div className="flex-row md:flex">
-                {candidates.map(c => (
+                {Voting.map(v => (
                     <img 
-                        src={c.img} 
-                        key={c.id} 
+                        src={v.img} 
+                        key={v.id} 
                         className={`rounded-3xl pb-1 mx-auto border border-4 ${c.selected ? 'border-secondary ' : ''} bg-primary`}
                         />
                 ))}
             </div>
             <button 
-                className="bg-[url('https://dummyimage.com/172x45/7BA9AC/fff&text=+')] text-white mt-5 mx-auto px-14 py-3 rounded-xl md:display: block">Submit</button>
+                className="bg-button text-white mt-5 mx-auto px-14 py-3 rounded-xl md:display: block" onClick={goToHasil}>Submit</button>
         </div>
     )
 }
